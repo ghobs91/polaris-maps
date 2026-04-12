b458b16888664125a1989211d97679fc551d30e8
ghobs91 <andrew@ghobrial.me>
2026-04-12 06:04:17 -0400

    got the app icon sorted out

--- ios/PolarisMaps/AppDelegate.h
+++ ios/PolarisMaps/AppDelegate.h
@@ -1,8 +1,7 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 #import <Expo/Expo.h>
-#import <CarPlay/CarPlay.h>
 
-@interface AppDelegate : EXAppDelegateWrapper <CPApplicationDelegate>
+@interface AppDelegate : EXAppDelegateWrapper
 
 @end
--- ios/PolarisMaps/CarPlayBridge.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#import <CarPlay/CarPlay.h>
-
-/// Plain C bridge so AppDelegate.mm never has to import PolarisMaps-Swift.h
-/// (which drags in ExpoModulesProvider → ModulesProvider and causes build errors).
-void PolarisCarPlayConnect(CPInterfaceController *interfaceController, CPWindow *window);
-void PolarisCarPlayDisconnect(void);
